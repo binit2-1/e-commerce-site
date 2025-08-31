bb614ba7e1211fb4371f0c279e347166c751b837
binit <binitgupta.1711@gmail.com>
2025-08-31 22:21:21 +0530

    initial react+vite+tailwindcss setup for shopping cart project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='w-full h-screen flex items-center justify-center bg-zinc-900'>
+      <div className='text-7xl text-center font-mono'>Hi React, Vite and Tailwind</div>
+    </div>
+  )
+}
+
+export default App
