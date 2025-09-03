9bbe8dfd664740539f553f9dc65c53c102bda5a8
binit <binitgupta.1711@gmail.com>
2025-09-03 17:21:51 +0530

    minor fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ import hamburger from '../assets/imgs/hamburger.svg'
 
 const Navbar = () => {
   return (
-    <div className='absolute w-full h-[70px] bg-white top-[40px] px-5 xl:px-6 2xl:px-20'>
+    <div className='absolute w-full h-[70px] bg-white top-[38px] px-5 xl:px-6 2xl:px-20'>
       <div className="flex justify-between lg:justify-center lg:px-20 xl:justify-around 2xl:justify-around h-full w-full items-center flex-row gap-5">
         <div className="flex items-center gap-4 lg:hidden xl:hidden 2xl:hidden">
           <img src={hamburger} alt="Menu" />
@@ -16,7 +16,7 @@ const Navbar = () => {
 
         <div className='hidden lg:block xl:block 2xl:block font-[neue-montreal-bold] lg:text-xl xl:text-2xl 2xl:text-3xl'>SHOP CO</div>
 
-        <div className='hidden lg:flex xl:flex 2xl:flex gap-3 lg:gap-4 xl:gap-5 2xl:gap-4'>
+        <div className='hidden lg:flex xl:flex 2xl:flex gap-3 lg:gap-4 xl:gap-5 2xl:gap-12'>
           <div className='font-[satoshi-medium] lg:text-[12px] xl:text-[13px] 2xl:text-[15px]'>Shop<img src={dropdown} alt="dropdown" className='inline-block ml-1' /></div>
           <div className='font-[satoshi-medium] lg:text-[12px] xl:text-[13px] 2xl:text-[15px]'>On Sale</div>
           <div className='font-[satoshi-medium] lg:text-[12px] xl:text-[13px] 2xl:text-[15px]'>New Arrivals</div>
